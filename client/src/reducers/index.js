be7e88265b6fca9e7b143573657d572dc7148325
Figaro <sweetfigaro27@gmail.com>
2020-11-15 22:40:14 +0800

    insert front end test page

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-
-import auth from './auth_reducer';
-import user from './user_reducer';
+import connections from './connections_reducer';
 
 const rootReducer = combineReducers({
-  auth,
-  user
+
+  connections,
 });
 
 export default rootReducer;
