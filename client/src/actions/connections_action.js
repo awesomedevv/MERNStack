473f65d3fb344cbb132d703512503835b0e488b5
passion1989 <wang198904@gmail.com>
2020-11-15 09:49:34 -0500

    client root change

--- client/src/actions/connections_action.js
+++ client/src/actions/connections_action.js
@@ -1,10 +1,7 @@
-import axios from 'axios';
-import {
-    CONNECTION_LIST
-} from './types';
+import axios from "axios";
+import { CONNECTION_LIST } from "./types";
 
-// const ROOT_URL = process.env.API_URI || 'http://localhost:8000';
-const ROOT_URL = 'http://192.168.109.230:8000';
+const ROOT_URL = process.env.API_URI || "http://localhost:8000";
 
 axios.defaults.baseURL = ROOT_URL;
 // if (localStorage.getItem('auth_jwt_token')) {
@@ -13,19 +10,18 @@ axios.defaults.baseURL = ROOT_URL;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export function getConnections(pageNum = 1) {
-    return function (dispatch) {
-        // Submit email/password to server
-        axios
-            .get(`/mynetwork/connections?page=${pageNum}`)
-            .then(res => {
-                dispatch({type: CONNECTION_LIST, payload: res.data})
-                // localStorage.setItem('connection_list', res.data);
-            })
-            .catch(error => {
-                console.log(error);
-
-            });
-    }
+  return function (dispatch) {
+    // Submit email/password to server
+    axios
+      .get(`/mynetwork/connections?page=${pageNum}`)
+      .then((res) => {
+        dispatch({ type: CONNECTION_LIST, payload: res.data });
+        // localStorage.setItem('connection_list', res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 }
 
 const request = axios;
